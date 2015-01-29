28f9dd00202a5ef0a14b8d2f5d2319e40b397bef
Rick Apichairuk <rick.apichairuk@gmail.com>
2015-01-29 18:06:17 +0700

    adding some basic tests for destination interface

--- lib/optimus_prime/destination.rb
+++ lib/optimus_prime/destination.rb
@@ -1,4 +1,11 @@
 module OptimusPrime
   class Destination 
+    def columns
+      raise "The 'columns' method is not defined in subclass! Please define before continuing."
+    end
+
+    def put_data
+      raise "The 'put_data' method is not defined in subclass! Please define before continuing."
+    end
   end
 end
