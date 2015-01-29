216a78902a26a09c4f13d027b5b65b7e81792025
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-01-29 15:28:38 +0700

    add vagrant

---
+++ Vagrantfile
@@ -0,0 +1,13 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.provision "shell", path: "script/provision.sh", privileged: false, keep_color: true
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.customize ["modifyvm", :id, "--memory", "2048"]
+  end
+end
---
+++ script/provision.sh
@@ -0,0 +1,27 @@
+#!/bin/bash --login
+
+set -e
+
+# Install packages
+sudo apt-get update --quiet
+sudo -E apt-get install --quiet --assume-yes curl git libpq-dev
+
+# Install rvm and ruby
+if ! rvm use ruby-2.2.0; then
+  # Install mpapis public key
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+
+  curl -sSL https://get.rvm.io | bash -s stable
+  source ~/.rvm/scripts/rvm
+  rvm install --quiet-curl ruby-2.2.0
+  rvm use ruby-2.2.0
+fi
+
+# cd straight to /vagrant on login
+if ! grep -q 'cd \/vagrant' ~/.bashrc; then
+  echo 'cd /vagrant' >> ~/.bashrc
+fi
+cd /vagrant
+
+# Install ruby dependencies
+bundle install
