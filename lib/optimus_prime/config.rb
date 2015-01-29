6b46c8bf2f8d6d87341d9972cf79039082eebf4f
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-01-29 15:26:43 +0700

    modified config.rb to raise error when yaml file is incorrect

--- lib/optimus_prime/config.rb
+++ lib/optimus_prime/config.rb
@@ -3,8 +3,8 @@ module OptimusPrime
     require 'yaml'
 
     # parse YAML file and make available as attributes or via getter method
-    attr_accessor :file_path
-   
+    attr_accessor :file_path, :sources, :transforms, :destinations, :unique_ids
+
     def initialize(file_path: nil)
       # add error checking to confirm that file_path is indeed a file
 
@@ -13,19 +13,54 @@ module OptimusPrime
         v = eval(k.to_s)
         instance_variable_set("@#{k}", v) unless v.nil?
       end
+
+      @unique_ids = { 
+        sources: {}, 
+        transforms: {}, 
+        destinations: {}
+      }
+
+      parse_config
     end
 
     def parse_config
-      puts 'HELLO!'
-      puts self.file_path
-
       yaml = YAML.load_file(self.file_path)
 
-      puts yaml.inspect
+      ['sources', 'transforms', 'destinations'].each do |type|
+        check_for_duplicates(type, yaml)
+      end
+
+      @sources = yaml['sources']
+      @transforms = yaml['transforms']
+      @destinations = yaml['destinations']
     end
 
-    def get_data
-    end 
+    def check_for_duplicates(type, yaml)
+      if yaml[type].is_a? Array
+        yaml[type].each do |item|
+          raise "#{type} duplicate!" unless is_identifier_unique?(type, item['source']['unique_identifier'])
+          self.mark_as_seen(type, item)
+        end
+      end
+    end
+
+    def get_sources
+      @sources
+    end
+
+    def get_source_by_id(unique_id)
+      @sources.each do |source|
+        return source if source['unique_identifier'] == unique_id
+      end
+    end
+
+    def mark_as_seen(type, item)
+      @unique_ids[type.to_sym][item['source']['unique_identifier']] = true
+    end
+
+    def is_identifier_unique?(type, id)
+      @unique_ids[type.to_sym][id].nil?
+    end
 
   end
 end
