52c39a7debf4b6d4978567ce3219baa26f976d1e
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-02-02 14:29:48 +0700

    add AppsFlyer spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 require 'rspec'
+require 'webmock/rspec'
+require 'csv'
+
 require File.expand_path("../../lib/optimus_prime.rb", __FILE__)
 require File.expand_path("../../lib/sources/csv.rb", __FILE__)
 require File.expand_path("../../lib/sources/json.rb", __FILE__)
 require File.expand_path("../../lib/sources/sqlite.rb", __FILE__)
+require File.expand_path("../../lib/sources/appsflyer.rb", __FILE__)
+WebMock.disable_net_connect!(allow_localhost: true)
+
+RSpec.configure do |c|
+  c.mock_with :rspec
+end
