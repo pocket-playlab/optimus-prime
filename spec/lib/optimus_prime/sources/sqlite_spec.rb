86eb5bb6b1c92a97cdb6f99e9ef79057a5eb15a6
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-02-03 11:59:01 +0700

    moved config on top of file

--- spec/lib/optimus_prime/sources/sqlite_spec.rb
+++ spec/lib/optimus_prime/sources/sqlite_spec.rb
@@ -36,8 +36,9 @@ describe "SQLite Source" do
 
   context "#retrieve_data" do
 
+    let(:config) { OptimusPrime::Config.new(file_path: "spec/supports/sources.yml") }
+
     context "configuration correct" do
-      let(:config) { OptimusPrime::Config.new(file_path: "spec/supports/sources.yml") }
       let(:sqlite_attributes) { config.get_source_by_id('sqlite3_game_level_database') }
       let(:sqlite_object) { Sqlite.new(sqlite_attributes['columns'], sqlite_attributes['file_path'], sqlite_attributes['query']) }
 
@@ -48,7 +49,6 @@ describe "SQLite Source" do
     end
 
     context "query incorrect" do
-      let(:config) { OptimusPrime::Config.new(file_path: "spec/supports/sources.yml") }
       let(:sqlite_attributes) { config.get_source_by_id('game_level_db_incorrect') }
       let(:sqlite_object) { Sqlite.new(sqlite_attributes['columns'], sqlite_attributes['file_path'], sqlite_attributes['query']) }
 
