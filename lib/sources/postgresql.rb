6f4c7cd9a253f2ab11df142c4720e5824970c350
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-02-03 18:39:39 +0700

    add postgres lib

---
+++ lib/sources/postgresql.rb
@@ -0,0 +1,30 @@
+require_relative '../optimus_init.rb'
+
+class PostgreSQL < OptimusPrime::Source::RDBMS
+
+  attr_accessor :columns, :db, :query
+
+  def initialize(columns, username, password, host, dbname, query)
+    raise 'columns required' unless columns
+    raise 'cannot connect database' unless host && username && password && dbname
+    raise 'query required' unless query
+    @columns = columns
+    @query = query
+
+    self.connect(username, password, host, dbname)
+  end
+
+  def connect(username, password, host, dbname)
+    @db = Sequel.connect(:adapter => 'postgres', :user => username, :host => host, :database => dbname,:password => password)
+    retrieve_data
+  end
+
+  def columns
+    return @columns
+  end
+
+  def execute_query
+    @db[@query]
+  end
+
+end
