affa9f24efdf23cb66191a78679761449350d312
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-02-04 18:06:54 +0700

    add test case for sum, max and min

--- spec/lib/optimus_prime/transform/group_by_spec.rb
+++ spec/lib/optimus_prime/transform/group_by_spec.rb
@@ -1,29 +1,59 @@
 require 'spec_helper'
 
 describe GroupBy do
-  
+
   let(:config) { OptimusPrime::Config.new(file_path: "spec/supports/sources.yml") }
-  let(:csv_source) { config.get_source_by_id('csv_transform_test') }
+  let(:csv_source) { config.get_transform_by_id('csv_transform_test') }
   let(:csv_instance) { Csv.new(csv_source['columns'], csv_source['file_path']) }
-  let(:key_columns) { ['country_code'] }
+  let(:key_columns) { ['id'] }
 
 
   context '#initialize' do
 
     context 'when invalid parameter type' do
-      it { expect { GroupBy.new(['array_of_source'], key_columns, 'sum') }.to raise_error("source must inherit from either OptimusPrime::Source or OptimusPrime::Transform!") }
-      it { expect { GroupBy.new(csv_instance, 'key_as_string', 'sum') }.to raise_error("key_columns should be an array") }
-      it { expect { GroupBy.new(csv_instance, key_columns, 100) }.to raise_error("100 strategy not include") }
-      it { expect { GroupBy.new(csv_instance, key_columns, 'plus') }.to raise_error("plus strategy not include") }
+      it { expect { GroupBy.new(['array_of_source'], key_columns, {'event_value' => 'sum'}) }.to raise_error("source must inherit from either OptimusPrime::Source or OptimusPrime::Transform!") }
+      it { expect { GroupBy.new(csv_instance, 'key_as_string', {'event_value' => 'sum'}) }.to raise_error("key_columns should be an array") }
+      it { expect { GroupBy.new(csv_instance, key_columns, {'event_value' => 100}) }.to raise_error("100 not include in strategies") }
+      it { expect { GroupBy.new(csv_instance, key_columns, {'event_value' => 'plus'}) }.to raise_error("plus not include in strategies") }
     end
 
     context 'when valid parameters' do
       it 'should exact source, key_columns and strategy' do
-        group_by = GroupBy.new(csv_instance, key_columns, 'sum')
+        group_by = GroupBy.new(csv_instance, key_columns, {'event_value' => 'sum'})
         expect(group_by.source).to eq(csv_instance)
-        expect(group_by.columns).to eq(key_columns.uniq)
-        expect(group_by.strategy).to eq('sum')
+        expect(group_by.strategies.keys.first).to eq('event_value')
+        expect(group_by.strategies.values.first).to eq('sum')
       end
+    end
+
+  end
+
+  context '#sum' do
+
+    let(:group_by) { GroupBy.new(csv_instance, key_columns, {'event_value' => 'sum'}) }
+
+    it 'should return sum value' do
+      expect(group_by.sum).to eq(1409)
+    end
+
+  end
+
+  context '#max' do
+
+    let(:group_by) { GroupBy.new(csv_instance, key_columns, {'event_value' => 'max'}) }
+
+    it 'should return primary key of maximum event_value' do
+      expect(group_by.max).to eq(9.to_s)
+    end
+
+  end
+
+  context '#min' do
+
+    let(:group_by) { GroupBy.new(csv_instance, key_columns, {'event_value' => 'min'}) }
+
+    it 'should return primary key of maximum event_value' do
+      expect(group_by.min).to eq(23.to_s)
     end
 
   end
