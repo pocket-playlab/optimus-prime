b432286214ecbc8f04ee08b6a1fb04200683267f
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-02-09 18:45:16 +0700

    removed rescue

--- lib/sources/sqlite.rb
+++ lib/sources/sqlite.rb
@@ -2,33 +2,21 @@ require_relative '../optimus_init.rb'
 
 class Sqlite < OptimusPrime::Source::RDBMS
 
-  attr_accessor :columns, :db, :db_path
+  attr_accessor :columns, :db, :query
 
   def initialize(columns, db_path, query)
     raise 'columns, db_path and query are required' unless columns && db_path && query
-    @columns = columns
+    super columns
     @query = query
 
     self.connect(db_path)
   end
 
   def connect(db_path)
-    begin
-      @db = Sequel.connect("sqlite://#{@db_path}")
-    rescue => e
-      raise "Can't connect database"
-    end
-  end
-
-  def columns
-    return @columns
+    @db = Sequel.connect("sqlite://#{db_path}")
   end
 
   def execute_query
-    begin
-      @db[@query]
-    rescue => e
-      raise e.message
-    end
+    @db[@query]
   end
 end
