baed2681ca974b35d05fcbfe3cc3bf40564917a6
Omar Khan <omar@omarkhan.me>
2015-02-27 09:37:00 +0700

    Move sources and destinations into their own modules

--- lib/optimus_prime.rb
+++ lib/optimus_prime.rb
@@ -1,10 +1,4 @@
 require 'optimus_prime/pipeline'
 require 'optimus_prime/step'
-
 require 'optimus_prime/source'
 require 'optimus_prime/destination'
-
-require 'sources/events_collector'
-require 'sources/appsflyer'
-
-require 'destinations/csv'
---
+++ lib/optimus_prime/sources/events_collector.rb
@@ -0,0 +1,47 @@
+require 'json'
+require 'stringio'
+require 'zlib'
+require 'aws-sdk'
+
+module OptimusPrime
+  module Sources
+    class EventsCollector < OptimusPrime::Source
+
+      def initialize(bucket:, from:, to:, **options)
+        @s3 = Aws::S3::Client.new(**options)
+        @bucket = bucket
+        @from = from
+        @to = to
+      end
+
+      def each
+        objects.each do |object|
+          gz = Zlib::GzipReader.new object.body
+          gz.each do |line|
+            yield JSON.parse line
+          end
+        end
+      end
+
+      private
+
+      def objects
+        keys.lazy.map do |key|
+          @s3.get_object bucket: @bucket, key: key
+        end
+      end
+
+      def keys
+        pages = @s3.list_objects bucket: @bucket
+        keys = pages.flat_map do |page|
+          page.contents.map(&:key).select do |key|
+            time = Time.parse key
+            time >= @from && time < @to
+          end
+        end
+        keys.sort!
+      end
+
+    end
+  end
+end
