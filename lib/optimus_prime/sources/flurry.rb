5e4dfa932a911653c6233a61613fe8bedab9c2d9
Rick Apichairuk <rick.apichairuk@gmail.com>
2015-03-02 12:04:23 +0700

    rubocop proposed changes

--- lib/optimus_prime/sources/flurry.rb
+++ lib/optimus_prime/sources/flurry.rb
@@ -5,7 +5,6 @@ require 'yajl'
 module OptimusPrime
   module Sources
     class Flurry < OptimusPrime::Source
-
       attr_accessor :start_date, :end_date
 
       def initialize(
@@ -44,7 +43,6 @@ module OptimusPrime
         end
       end
 
-
       #
       # Build the url to make a request for a report to
       # This is not the same url as the report data itself
@@ -53,10 +51,10 @@ module OptimusPrime
       #
       def request_report_url
         params = {
-            apiAccessCode: @api_access_code,
-            apiKey:        @api_key,
-            startTime:     convert_date_to_unix_ts(@start_date) * 1000,
-            endTime:       convert_date_to_unix_ts(@end_date) * 1000,
+          apiAccessCode: @api_access_code,
+          apiKey:        @api_key,
+          startTime:     convert_date_to_unix_ts(@start_date) * 1000,
+          endTime:       convert_date_to_unix_ts(@end_date) * 1000,
         }
 
         query_string = params.map { |k, v| "#{k}=#{v}" }.join('&')
@@ -73,7 +71,7 @@ module OptimusPrime
         end
       end
 
-      #private
+      private
 
       # basically execute same thing as the upload_reports_to_s3 method fetch_reports.rb
       # with the exception that there is no S3 related code. Only concerned about getting the data.
@@ -85,16 +83,16 @@ module OptimusPrime
         end_time   = convert_to_utc_time @end_date
 
         report = request_report(start_time, end_time)
-        unless report.has_key? 'report'
+        unless report.key? 'report'
           dump = JSON.dump report
           logger.error dump
-          raise "failed to receive report json!"
+          raise 'failed to receive report json!'
         end
 
         report_uri = report['report']['@reportUri']
         unless report_uri
           logger.error 'No report uri'
-          raise "No report uri!"
+          raise 'No report uri!'
         end
 
         sleep @poll_interval # Wait so we don't get rate-limited
@@ -103,7 +101,7 @@ module OptimusPrime
         if report_data.nil?
           logger.error "Missing data for interval: #{start_time} - #{end_time}"
         else
-          hashes = time_call("Processing events") { process_events report_data }
+          hashes = time_call('Processing events') { process_events report_data }
         end
 
         logger.info('Done')
@@ -111,11 +109,14 @@ module OptimusPrime
       end
 
       #
-      # This makes the initial request for the report. The data is not returned. Only a json response is returned.
-      # The response should have the url to retrieve the actual resulting data from the request.
+      # This makes the initial request for the report. The data is not returned. Only a json
+      # response is returned. The response should have the url to retrieve the actual
+      # resulting data from the request.
       #
       def request_report(from, to)
-        raise ArgumentError.new 'Invalid date range' unless from.kind_of? Time and to.kind_of? Time and from < to
+        unless from.is_a? Time and to.is_a? Time and from < to
+          raise ArgumentError.new 'Invalid date range'
+        end
 
         url = request_report_url
         logger.debug("Request_report url is #{url}")
@@ -124,19 +125,18 @@ module OptimusPrime
 
         content_type = response.header['content-type']
 
-        if content_type != "application/json"
-          logger.error "Error getting report"
-          logger.error "Headers:"
+        if content_type != 'application/json'
+          logger.error 'Error getting report'
+          logger.error 'Headers:'
           logger.error response.header.to_s
-          logger.error "Body:"
+          logger.error 'Body:'
           logger.error response.body
-          raise "Request report failed!"
+          raise 'Request report failed!'
         end
 
         logger.debug response.body
         Yajl::Parser.parse response.body
       end
-
 
       #
       # This method actually goes out to retrieve the data that resulted from the query.
@@ -144,35 +144,34 @@ module OptimusPrime
       # at which point, it will raise an exception.
       #
       def get_report_data(report_uri, retries=0)
-        logger.debug "Entered get_report_data method"
+        logger.debug 'Entered get_report_data method'
         response = Net::HTTP.get_response URI(report_uri)
         content_type = response.header['content-type']
         case content_type
-          when 'application/json'
-            raise 'Unknown response' unless Yajl::Parser.parse(response.body)['@reportReady'] == 'false'
-            logger.debug("Report not ready, retrying in #{@poll_interval} seconds")
-            sleep @poll_interval
-            get_report_data report_uri
-          when 'application/octet-stream'
-            #self.logger.info "Report ready: #{report_uri}"
-            logger.info "Report ready: #{report_uri}"
-            # If we get here, SUCCESS!
-            Yajl::Parser.parse Zlib::GzipReader.new(StringIO.new(response.body)).read
+        when 'application/json'
+          raise 'Unknown response' unless Yajl::Parser.parse(response.body)['@reportReady'] == 'false'
+          logger.debug("Report not ready, retrying in #{@poll_interval} seconds")
+          sleep @poll_interval
+          get_report_data report_uri
+        when 'application/octet-stream'
+          #self.logger.info "Report ready: #{report_uri}"
+          logger.info "Report ready: #{report_uri}"
+          # If we get here, SUCCESS!
+          Yajl::Parser.parse Zlib::GzipReader.new(StringIO.new(response.body)).read
+        else
+          headers = response.each_header.map { |k, v| "#{k}: #{v}" }.join("\n")
+          logger.error "Response:\n#{headers}\n\n#{response.body[0...1000]}"
+          logger.error "Unknown content-type: #{content_type}"
+          # Retry 3 times before we let it die
+          if retries <= 3
+            # Make sure to sleep if we are being rate limited.
+            sleep POLL_INTERVAL
+            get_report_data(report_uri, retries+1)
           else
-            headers = response.each_header.map { |k, v| "#{k}: #{v}" }.join("\n")
-            logger.error "Response:\n#{headers}\n\n#{response.body[0...1000]}"
-            logger.error "Unknown content-type: #{content_type}"
-            # Retry 3 times before we let it die
-            if retries <= 3
-              # Make sure to sleep if we are being rate limited.
-              sleep POLL_INTERVAL
-              get_report_data(report_uri, retries+1)
-            else
-              nil
-            end
+            nil
+          end
         end
       end
-
 
       #
       # Given an events report, returns an enumerator that yields a hash containing
@@ -190,11 +189,11 @@ module OptimusPrime
           report_data['sessionEvents'].lazy.map(&expand_keys).each do |session|
             session['logs'].each do |event|
               data = {
-                  'Session'   => session['uniqueId'],
-                  'Version'   => session['version'],
-                  'Device'    => session['device'],
-                  'Event'     => event['eventName'],
-                  'Timestamp' => session['startTimestamp'] + event['offsetTimestamp'],
+                'Session'   => session['uniqueId'],
+                'Version'   => session['version'],
+                'Device'    => session['device'],
+                'Event'     => event['eventName'],
+                'Timestamp' => session['startTimestamp'] + event['offsetTimestamp'],
               }
               data.merge!(event['parameters']) if event['parameters']
               enum.yield(data)
@@ -213,16 +212,15 @@ module OptimusPrime
         Time.utc(date.year, date.month, date.day)
       end
 
-
       def convert_date_to_unix_ts(date)
-        epoch = Date.new(1970,1,1)
+        epoch = Date.new(1970, 1, 1)
         date - epoch
         date.to_time.to_i
       end
 
       def format_timing(timing)
         if timing < 1
-          "#{timing*1000} ms"
+          "#{timing * 1000} ms"
         else
           "#{timing} s"
         end
@@ -237,7 +235,6 @@ module OptimusPrime
         logger.info "#{str}: #{format_timing(end_time - start_time)}"
         result
       end
-
     end
   end
 end
