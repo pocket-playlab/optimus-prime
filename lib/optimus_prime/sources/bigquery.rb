04e0d2bc0079334233fb58a4c35198cb18cb0668
Rinlapat Pu <prair@rinlapat.me>
2015-03-02 17:58:51 +0700

    #map_result_into_rows should return nil if field’s value is nil.

--- lib/optimus_prime/sources/bigquery.rb
+++ lib/optimus_prime/sources/bigquery.rb
@@ -62,16 +62,20 @@ module OptimusPrime
       def map_result_into_rows(fields, rows)
         rows.map do |row|
           Hash[row['f'].map.with_index do |field, index|
-            value = case fields[index]['type']
-                    when 'INTEGER'
-                      field['v'].to_i
-                    when 'FLOAT'
-                      field['v'].to_f
-                    when 'BOOLEAN'
-                      field['v'] == 'true'
-                    else
-                      field['v']
-                    end
+            value = if field['v'].nil?
+              field['v']
+            else
+              case fields[index]['type']
+              when 'INTEGER'
+                field['v'].to_i
+              when 'FLOAT'
+                field['v'].to_f
+              when 'BOOLEAN'
+                field['v'] == 'true'
+              else
+                field['v']
+              end
+            end
             [fields[index]['name'].to_sym, value]
           end]
         end
