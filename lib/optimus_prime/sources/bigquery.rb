40096baad1c581cc612049c048112e38701905e7
Rinlapat Pu <prair@rinlapat.me>
2015-03-03 11:39:59 +0700

    [Bigquery source] log error if #query or #get_query_results is failed.

--- lib/optimus_prime/sources/bigquery.rb
+++ lib/optimus_prime/sources/bigquery.rb
@@ -27,8 +27,12 @@ module OptimusPrime
         end
       end
 
-      def query(sql)
-        result = GoogleBigquery::Jobs.query @project_id, query: sql
+      def query
+        begin
+          result = GoogleBigquery::Jobs.query @project_id, query: @sql
+        rescue => e
+          raise_error "Bigquery#query - #{e}", "@project_id = #{@project_id} | @sql = #{@sql}"
+        end
         if result['jobComplete'] && result['pageToken'].nil?
           return map_result_into_rows result['schema']['fields'], result['rows']
         end
@@ -40,7 +44,11 @@ module OptimusPrime
         rows = []
         request_opt = {}
         loop do
-          result = GoogleBigquery::Jobs.getQueryResults @project_id, job_id, request_opt
+          begin
+            result = GoogleBigquery::Jobs.getQueryResults @project_id, job_id, request_opt
+          rescue => e
+            raise_error "Bigquery#get_query_results - #{e}", "#@project_id = #{@project_id} | job_id = #{job_id} | request_opt = #{request_opt}"
+          end
           if result['jobComplete']
             rows.concat(map_result_into_rows(result['schema']['fields'], result['rows'])).uniq!
             request_opt = { pageToken: result['pageToken'] } unless result['pageToken'].nil?
@@ -74,6 +82,11 @@ module OptimusPrime
             [fields[index]['name'].to_sym, value]
           end]
         end
+      end
+
+      def raise_error(error, params)
+        logger.error "#{error} | #{params}"
+        raise error
       end
     end
   end
