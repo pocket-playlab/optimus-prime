f5b4919cf150664d71a81fe9560e71cb4b662cc3
Rinlapat Pu <prair@rinlapat.me>
2015-03-05 16:43:33 +0700

    Flurry Timestamp will be converted to second instead of millisecond.

--- lib/optimus_prime/sources/flurry.rb
+++ lib/optimus_prime/sources/flurry.rb
@@ -183,12 +183,13 @@ module OptimusPrime
         Enumerator.new do |enum|
           report_data['sessionEvents'].lazy.map(&expand_keys).each do |session|
             session['logs'].each do |event|
+              timestamp = session['startTimestamp'] + event['offsetTimestamp']
               data = {
                 'Session'   => session['uniqueId'],
                 'Version'   => session['version'],
                 'Device'    => session['device'],
                 'Event'     => event['eventName'],
-                'Timestamp' => session['startTimestamp'] + event['offsetTimestamp'],
+                'Timestamp' => Time.strptime(timestamp.to_s, '%Q').utc.to_i,
               }
               data.merge!(event['parameters']) if event['parameters']
               enum.yield(data)
