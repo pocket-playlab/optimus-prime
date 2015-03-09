caf36d1913d43bd4d54922fd03be7cd6cd0cc1ed
Rick Apichairuk <rick.apichairuk@gmail.com>
2015-03-09 17:16:25 +0700

    adding aruba config

---
+++ spec/support/aruba.rb
@@ -0,0 +1,13 @@
+require 'aruba'
+require 'aruba/api'
+require 'aruba/reporting'
+
+
+RSpec.configure do |config|
+  config.include Aruba::Api
+
+  config.before(:each) do
+    restore_env
+    clean_current_dir
+  end
+end
