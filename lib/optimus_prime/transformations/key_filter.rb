9787486f5ae2b22ab9a968b931a3197914649c25
Tamer Shlash <mr.tamershlash@gmail.com>
2015-03-13 15:29:47 +0700

    added key_filter: a transformer that filters keys in a hash record

---
+++ lib/optimus_prime/transformations/key_filter.rb
@@ -0,0 +1,26 @@
+require 'optimus_prime'
+require 'optimus_prime/destination'
+
+module OptimusPrime
+  module Destinations
+    class KeyFilter < Destination
+      # This class accepts a hash object and removes pairs
+      # whose key is not included in the array given in the
+      # initializer.
+
+      def initialize(fields:)
+        @fields = fields
+      end
+
+      def write(record)
+        push transform(record)
+      end
+
+      private
+
+      def transform(record)
+        record.select { |key, value| @fields.include? key }
+      end
+    end
+  end
+end
