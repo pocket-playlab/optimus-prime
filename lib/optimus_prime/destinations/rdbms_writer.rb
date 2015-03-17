2bdc7dc5bfc4a2a09e0d1f17b1b664eb13ed5bdc
Thibault Denizet <thi.denizet@gmail.com>
2015-03-17 14:26:52 +0700

    Rename RDBMS to RDBMS writer

---
+++ lib/optimus_prime/destinations/rdbms_writer.rb
@@ -0,0 +1,25 @@
+module OptimusPrime
+  module Destinations
+    class RdbmsWriter < Destination
+      def initialize(dsn:, table:, **options)
+        @db = Sequel.connect(dsn, **options)
+        @table = @db[table.to_sym]
+      end
+
+      def write(record)
+        # log any data that doesn't match what we are expecting
+        # not sure if this should be a fatal condition...
+        unless record.is_a?(::Hash)
+          logger.error 'record was not a Hash as expected!'
+          logger.error record.inspect
+        end
+
+        @table.insert record
+      end
+
+      def finish
+        @db.disconnect
+      end
+    end
+  end
+end
