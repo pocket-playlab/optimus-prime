41c5d4e21090b2666545312aed851c03c85a5381
Thibault Denizet <thi.denizet@gmail.com>
2015-03-17 14:28:03 +0700

    Add RDBMS Destination v2

--- lib/optimus_prime.rb
+++ lib/optimus_prime.rb
@@ -1,7 +1,12 @@
+require 'sequel'
+require 'pry-byebug'
+
 require 'optimus_prime/pipeline'
 require 'optimus_prime/step'
 require 'optimus_prime/source'
 require 'optimus_prime/destination'
+
+require 'optimus_prime/destinations/rdbms_writer'
 
 # Load all Sources and Destinations
 Dir[File.dirname(__FILE__) + '/optimus_prime/**/*.rb'].each do |file|
--- spec/optimus_prime/destinations/rdbms_spec.rb
+++ spec/optimus_prime/destinations/rdbms_spec.rb
@@ -1,23 +1,30 @@
 require 'spec_helper'
-require 'optimus_prime/destinations/rdbms'
 
 RSpec.describe OptimusPrime::Destinations::Rdbms do
   let(:input) do
     [
-      { name:  'Rick',     car:  'C6 Z06',             horsepower:  505  },
-      { name:  'Omar',     car:  'Range Rover',        horsepower:  280  },
-      { name:  'Prair',    car:  'Toyota Camry',       horsepower:  160  },
-      { name:  'M',        car:  'Honda Civic Type R', horsepower:  750  },
-      { name:  'Thibault', car:  'Audi S4',            horsepower:  480  },
-      { name:  'Tamer',    car:  'Mercedes SLK',       horsepower:  350  },
+      { platform: 'ios', version: '1.0.1' },
+      { platform: 'android', version: '1.0.1'  },
+      { platform: 'android', version: '1.0.2'  },
+      { platform: 'android', version: '1.0.1'  }
     ]
   end
 
   let(:dsn) { 'sqlite://test.db' }
-  let(:table) { :developer_cars }
+  let(:table_name) { :events }
+  let(:db) { Sequel.connect(dsn) }
+  let(:table) { db[table_name.to_sym] }
 
-  let(:destination) do
-    OptimusPrime::Destinations::Rdbms.new dsn: dsn, table: table, sql_trace: false
+  let(:destination_with_string_condition) do
+    OptimusPrime::Destinations::Rdbms.new dsn: dsn, table: table_name,
+                                          conditions: "Platform = 'ios'",
+                                          sql_trace: false
+  end
+
+  let(:destination_with_hash_condition) do
+    OptimusPrime::Destinations::Rdbms.new dsn: dsn, table: table_name,
+                                          conditions: { version: '1.0.1' },
+                                          sql_trace: false
   end
 
   before do
@@ -25,26 +32,51 @@ RSpec.describe OptimusPrime::Destinations::Rdbms do
     # if you need to debug or trace sql, uncomment following lines
     # db.loggers << Logger.new($stdout)
     # db.sql_log_level = :debug
-    db.drop_table? table
-    db.create_table table do
-      String :name
-      String :car
-      Integer :horsepower
+    db.drop_table? table_name
+    db.create_table table_name do
+      String :platform
+      String :version
     end
   end
 
   def insert_records
+    input.each { |record| table.insert record }
+  end
+
+  def insert_records_with_destination(destination)
     input.each { |record| destination.write record }
     destination.close
   end
 
   def records_from_db
-    db = Sequel.connect(dsn)
-    db[table].all
+    table.all
   end
 
-  it 'should upload insert records into database' do
-    insert_records
-    expect(records_from_db).to eq(input)
+  context 'string condition' do
+    it 'should delete all the records with ios platform' do
+      insert_records
+      expect(table.where(platform: 'ios').count).to eq 1
+      insert_records_with_destination(destination_with_string_condition)
+      expect(records_from_db.count).to eq 7
+    end
+
+    it 'should upload insert records into database' do
+      insert_records_with_destination(destination_with_string_condition)
+      expect(records_from_db).to eq(input)
+    end
+  end
+
+  context 'hash condition' do
+    it 'should delete all the records with version 1.0.1' do
+      insert_records
+      expect(table.where(version: '1.0.1').count).to eq 3
+      insert_records_with_destination(destination_with_hash_condition)
+      expect(records_from_db.count).to eq 5
+    end
+
+    it 'should upload insert records into database' do
+      insert_records_with_destination(destination_with_hash_condition)
+      expect(records_from_db).to eq(input)
+    end
   end
 end
