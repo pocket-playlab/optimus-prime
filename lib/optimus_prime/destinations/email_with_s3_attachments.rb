a87027a9a6a08e37d01ea5d0b86081c0583cf8c3
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-03-20 17:16:31 +0700

    change email_config

--- lib/optimus_prime/destinations/email_with_s3_attachments.rb
+++ lib/optimus_prime/destinations/email_with_s3_attachments.rb
@@ -3,8 +3,8 @@ require 'mail'
 module OptimusPrime
   module Destinations
     class EmailWithS3Attachments < Destination
-      def initialize(sender:, recipients:, title:, body:)
-        set_default
+      def initialize(sender:, recipients:, title:, body:, email_config:)
+        set_default email_config
         @mail = Mail.new do
           from sender
           to recipients
@@ -13,30 +13,31 @@ module OptimusPrime
         end
       end
 
-      def set_default
+      def set_default(email_config)
         Mail.defaults do
-          delivery_method :smtp, address: ENV.fetch('EMAIL_HOST'),
-                                 port: ENV.fetch('EMAIL_PORT'),
-                                 domain: ENV.fetch('EMAIL_DOMAIN'),
-                                 user_name: ENV.fetch('EMAIL_USERNAME'),
-                                 password: ENV.fetch('EMAIL_PASSWORD'),
-                                 authentication: 'plain',
-                                 enable_starttls_auto: true
+          delivery_method email_config[:method] || :smtp,
+                          address: email_config[:host],
+                          port: email_config[:port],
+                          domain: email_config[:domain],
+                          user_name: email_config[:user_name],
+                          password: email_config[:password],
+                          authentication: 'plain',
+                          enable_starttls_auto: true
         end
       end
 
       def write(s3_config)
         s3_file = download bucket: s3_config[:bucket], key: s3_config[:key]
-        @mail.add_file :filename => s3_config[:key], :content => s3_file.body.read
+        @mail.add_file filename: s3_config[:key], content: s3_file.body.read
       end
 
       def download(bucket:, key:)
         s3 ||= Aws::S3::Client.new
-        s3.get_object({ bucket: bucket, key: key })
+        s3.get_object(bucket: bucket, key: key)
       end
 
       def finish
-        @mail.deliver!
+        @mail.deliver
       end
     end
   end
