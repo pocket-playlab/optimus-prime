64cd9a0c0cd5e43ff021cf73cfadc65d745a42c0
Tamer Shlash <mr.tamershlash@gmail.com>
2015-03-25 18:17:10 +0700

    log all insertion errors, not only their count

--- lib/optimus_prime/destinations/common/bigquery_table_base.rb
+++ lib/optimus_prime/destinations/common/bigquery_table_base.rb
@@ -62,6 +62,7 @@ module OptimusPrime
                            body:   { 'kind' => 'bigquery#tableDataInsertAllRequest',
                                      'rows' => buffer }
         body = JSON.parse response.body
+        body.fetch('insertErrors', []).each { |err| logger.error err }
         failed = body.fetch('insertErrors', []).map { |err| err['index'] }.uniq.length
         raise "Failed to insert #{failed} row(s) to table #{id}" if failed > 0
       end
