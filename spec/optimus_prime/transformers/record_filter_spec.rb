620c6b12456c9fb777be20136f25c5d45a50778b
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-04-01 16:17:11 +0700

    add more spec on record_filter_spec to support new filter(contained, not_contained)

--- spec/optimus_prime/transformers/record_filter_spec.rb
+++ spec/optimus_prime/transformers/record_filter_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe OptimusPrime::Transformers::RecordFilter do
       level: { type: :range, values: [0, 100] },
       altitude: { type: :less_than_or_equal, values: [8000] },
       character: { type: :set, values: ['tom', 'jerry'] },
-      nil_stuff: { type: :set, values: [nil] }
+      nil_stuff: { type: :set, values: [nil] },
     }
   end
 
@@ -57,4 +57,40 @@ RSpec.describe OptimusPrime::Transformers::RecordFilter do
     expect(output).to match_array output_invalid
   end
 
+  context '#contained, #not_contained' do
+    let(:records) do
+      [
+        { code: 'A-111-3CB' },
+        { code: 'B-222-3CB' },
+        { code: 'A-333-3CB' },
+        { code: 'C-444-3CB' },
+        { code: 'C-555-3CB' }
+      ]
+    end
+
+    let(:start_with_a_or_c) { { code: { type: :not_contained, values: ['B-'] } } }
+    let(:start_with_b) { { code: { type: :contained, values: ['B-'] } } }
+
+    let(:records_without_b) do
+      [
+        { code: 'A-111-3CB' },
+        { code: 'A-333-3CB' },
+        { code: 'C-444-3CB' },
+        { code: 'C-555-3CB' }
+      ]
+    end
+
+    let(:ac_filter) { OptimusPrime::Transformers::RecordFilter.new(constraints: start_with_a_or_c) }
+    let(:b_filter) { OptimusPrime::Transformers::RecordFilter.new(constraints: start_with_b) }
+
+    it 'should not contained b' do
+      output = write_records(ac_filter, records)
+      expect(output).to match_array records_without_b
+    end
+
+    it 'should contained only b' do
+      output = write_records(b_filter, records)
+      expect(output).to match_array [{ code: 'B-222-3CB' }]
+    end
+  end
 end
