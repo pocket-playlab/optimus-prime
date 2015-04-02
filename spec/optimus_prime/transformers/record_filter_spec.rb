9e657cc62d8df0997600ea6980902c850c37d2d1
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-04-02 16:49:55 +0700

    change keys from string to symbol

--- spec/optimus_prime/transformers/record_filter_spec.rb
+++ spec/optimus_prime/transformers/record_filter_spec.rb
@@ -16,27 +16,27 @@ RSpec.describe OptimusPrime::Transformers::RecordFilter do
 
   let(:input_valid) do
     [
-      { character: 'tom',   level: 50, altitude: 5612 },
-      { character: 'jerry', level: 50, altitude: 4581 },
-      { character: 'jerry', level: 10, altitude: 2345 }
+      { 'character' => 'tom',   'level' => 50, 'altitude' => 5612 },
+      { 'character' => 'jerry', 'level' => 50, 'altitude' => 4581 },
+      { 'character' => 'jerry', 'level' => 10, 'altitude' => 2345 }
     ]
   end
 
   let(:input_invalid) do
     [
-      { character: 'tom',   level: 150, altitude: 5612 },
-      { character: 'jerry', level: 50, altitude: 4581 },
-      { character: 'jerry', level: 50, altitude: 4582, nil_stuff: nil },
-      { character: 'jerry', level: 50, altitude: 8001 },
-      { character: 'jerry', level: 2, altitude: 1000, nil_stuff: 'not_nil!' },
-      { character: 'james', level: 50, altitude: 4581 }
+      { 'character' => 'tom',   'level' => 150, 'altitude' => 5612 },
+      { 'character' => 'jerry', 'level' => 50, 'altitude' => 4581 },
+      { 'character' => 'jerry', 'level' => 50, 'altitude' => 4582, 'nil_stuff' => nil },
+      { 'character' => 'jerry', 'level' => 50, 'altitude' => 8001 },
+      { 'character' => 'jerry', 'level' => 2, 'altitude' => 1000, 'nil_stuff' => 'not_nil!' },
+      { 'character' => 'james', 'level' => 50, 'altitude' => 4581 }
     ]
   end
 
   let(:output_invalid) do
     [
-      { character: 'jerry', level: 50, altitude: 4582, nil_stuff: nil },
-      { character: 'jerry', level: 50, altitude: 4581 }
+      { 'character' => 'jerry', 'level' => 50, 'altitude' => 4582, 'nil_stuff' => nil },
+      { 'character' => 'jerry', 'level' => 50, 'altitude' => 4581 }
     ]
   end
 
@@ -60,11 +60,11 @@ RSpec.describe OptimusPrime::Transformers::RecordFilter do
   context '#contained, #not_contained' do
     let(:records) do
       [
-        { code: 'A-111-3CB' },
-        { code: 'B-222-3CB' },
-        { code: 'A-333-3CB' },
-        { code: 'C-444-3CB' },
-        { code: 'C-555-3CB' }
+        { 'code' => 'A-111-3CB' },
+        { 'code' => 'B-222-3CB' },
+        { 'code' => 'A-333-3CB' },
+        { 'code' => 'C-444-3CB' },
+        { 'code' => 'C-555-3CB' }
       ]
     end
 
@@ -73,10 +73,10 @@ RSpec.describe OptimusPrime::Transformers::RecordFilter do
 
     let(:records_without_b) do
       [
-        { code: 'A-111-3CB' },
-        { code: 'A-333-3CB' },
-        { code: 'C-444-3CB' },
-        { code: 'C-555-3CB' }
+        { 'code' => 'A-111-3CB' },
+        { 'code' => 'A-333-3CB' },
+        { 'code' => 'C-444-3CB' },
+        { 'code' => 'C-555-3CB' }
       ]
     end
 
@@ -90,7 +90,43 @@ RSpec.describe OptimusPrime::Transformers::RecordFilter do
 
     it 'should contained only b' do
       output = write_records(b_filter, records)
-      expect(output).to match_array [{ code: 'B-222-3CB' }]
+      expect(output).to match_array [{ 'code' => 'B-222-3CB' }]
     end
+  end
+
+  context 'stringify' do
+    let(:record_filter) { OptimusPrime::Transformers::RecordFilter }
+
+    let(:symbol_key) { [ { name: 'Bob' } ] }
+    let(:string_key) { [ { 'name' => 'Bob' } ] }
+
+    let(:condition) { { name: { type: :set, values: ['Bob'] } } }
+
+    context 'string keys' do
+      it 'should keep key as string and match the condition' do
+        out = write_records(record_filter.new(constraints: condition, stringify: true), string_key)
+        expect(out).to match_array string_key
+      end
+
+      it 'should filter the records when stringify is false' do
+        # expect (write_records(record_filter.new(constraints: condition, stringify: false), string_key)).to raise_error
+        # require 'byebug'; byebug
+        # record_filter.new(constraints: condition, stringify: false).write(string_key)
+        # expect(out).to match_array []
+      end
+    end
+
+    context 'symbol keys' do
+      it 'should filter the records' do
+        out = write_records(record_filter.new(constraints: condition, stringify: true), symbol_key)
+        expect(out).to match_array []
+      end
+
+      it 'should match symbol key' do
+        # out = write_records(record_filter.new(constraints: condition, stringify: false), symbol_key)
+        # expect(out).to match_array symbol_key
+      end
+    end
+
   end
 end
