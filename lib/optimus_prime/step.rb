58dd40a6923e1f1c516294c558742b46a0782a5f
Omar Khan <omar@omarkhan.me>
2015-04-03 16:40:26 +0700

    Document Pipeline and Step implementations

--- lib/optimus_prime/step.rb
+++ lib/optimus_prime/step.rb
@@ -1,8 +1,24 @@
 require 'logger'
 
 module OptimusPrime
+  # The base class for all data processing steps. Do not subclass `Step`
+  # directly, subclass `OptimusPrime::Source` or `OptimusPrime::Destination`
+  # instead.
+  #
+  # Set up a step by adding input and ouput queues like this:
+  #
+  #     step.input  << inqueue
+  #     step.output << outqueue
+  #
+  # A step must have at least one input or output queue. Once these have been
+  # configured, start the step by calling `#start`. The step will then listen
+  # on the input queues, passing each value received to the `#process` method.
+  #
+  # The step will finish processing data when it receives a falsy value (`nil`
+  # or `false`).
   class Step
     class << self
+      # Keep track of all subclasses so we can retrieve them by name.
       def inherited(subclass)
         descendants << subclass
       end
@@ -51,6 +67,8 @@ module OptimusPrime
     end
     alias_method :wait, :join
 
+    # Call the `finish` callback and push `nil` to the output queues to signal
+    # the end of the data stream.
     def close
       @closed = true
       finish
@@ -61,24 +79,32 @@ module OptimusPrime
       not threads.empty?
     end
 
+    # Has this step reached the end of its input?
     def closed?
       @closed || false
     end
 
+    # Has this step finished processing all its data?
     def finished?
       started? and threads.none?(&:status)
     end
 
+    # The set of input queues. Anything that responds to `#pop` can be added
+    # to this set.
     def input
       @input ||= Set.new
     end
 
+    # The set of output queues. Anything that responds to `#push` can be added
+    # to this set.
     def output
       @output ||= Set.new
     end
 
     private
 
+    # Spawn a listener thread for each input queue, then call `#close` when
+    # they have all finished.
     def listen
       consumers = input.map do |queue|
         consume queue
