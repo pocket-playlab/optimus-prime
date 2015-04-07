84ff38ea2851650e2d9d32fea42847e6e9a3e480
Tamer Shlash <mr.tamershlash@gmail.com>
2015-04-07 15:25:17 +0700

    added pipeline operator to run pipeline config files

---
+++ lib/optimus_prime/cli/pipeline_operator.rb
@@ -0,0 +1,47 @@
+require_relative 'optimus_operator'
+
+module OptimusPrime
+  module CLI
+    # This class wraps and runs a pipeline
+    class PipelineOperator < OptimusOperator
+      attr_accessor :name, :pipeline
+      attr_reader :cli_dependencies
+
+      def initialize(config_file, name, cli_dependencies = '')
+        super(config_file)
+        if config.key? name
+          self.name = name
+        else
+          raise "Pipeline #{name} does not exist in #{config_file}"
+        end
+        self.pipeline = OptimusPrime::Pipeline.new graph
+        @cli_dependencies = cli_dependencies ? cli_dependencies.split(',') : []
+        require_dependencies
+      end
+
+      def operate
+        pipeline.start
+        pipeline.wait
+        pipeline.finished? ? puts('Pipeline finished.') : raise('Pipeline failed to finish!')
+      end
+
+      def finished?
+        pipeline.finished?
+      end
+
+      private
+
+      def graph
+        @graph ||= config[name]['graph'].symbolize_nested_keys
+      end
+
+      def require_dependencies
+        yaml_dependencies = config[name]['dependencies'] || []
+        (cli_dependencies + yaml_dependencies).uniq.each do |dependency|
+          p "Requiring #{dependency}"
+          require(dependency)
+        end
+      end
+    end
+  end
+end
