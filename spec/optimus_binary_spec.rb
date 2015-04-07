373d9d62863265fa9e426ec1eec3303de5b40ee2
Tamer Shlash <mr.tamershlash@gmail.com>
2015-04-07 18:22:13 +0700

    added quick test cases for optimus factory

--- spec/optimus_binary_spec.rb
+++ spec/optimus_binary_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
+# TODO: replace these tests with Thor-compatible tests
+# see https://github.com/backup/backup/blob/master/spec/cli_spec.rb
+# and https://github.com/erikhuda/thor/tree/master/spec
+
 root = Pathname.new(__FILE__).parent.parent
 ENV['PATH'] = "#{root.join('bin')}:#{ENV['PATH']}"
 
@@ -86,5 +90,69 @@ Pipeline finished.
       output = `#{operate} pipeline #{config_path} #{pipeline_name} 2>&1`
       expect(output).to include("Pipeline #{pipeline_name} does not exist in #{config_path}")
     end
+  end
+
+  context 'when running factory' do
+    let(:factory_log) { '/tmp/factory.log' }
+    before { File.delete(factory_log) if File.exist?(factory_log) }
+
+    def execute(command)
+      `#{command} > #{factory_log} 2>&1`
+    end
+
+    def output
+      File.read(factory_log)
+    end
+
+    context 'without arguments' do
+      it 'displays error and help messages' do
+        execute("#{operate} factory")
+        expect(output).to include('ERROR: "optimus factory" was called with no arguments',
+                                  'Usage: "optimus factory <file>"')
+      end
+    end
+
+    context 'with single pipeline' do
+      let(:factory_single) { 'spec/supports/config/factory-single.yml' }
+      it 'operates correctly' do
+        execute("#{operate} factory #{factory_single}")
+        expect(output).to include('Starting pipeline', 'Pipeline finished', 'Factory finished.')
+      end
+    end
+
+    context 'with two independent pipelines' do
+      let(:independent_success) { 'spec/supports/config/factory-independent-success.yml' }
+      it 'runs the second pipeline if the first finished successfully' do
+        execute("#{operate} factory #{independent_success}")
+        expect(output).to include('first finished with exit code 0',
+                                  'Starting pipeline second')
+      end
+
+      let(:independent_failure) { 'spec/supports/config/factory-independent-failure.yml' }
+      it 'runs the second pipeline even if the first finished unsuccessfully' do
+        execute("#{operate} factory #{independent_failure}")
+        expect(output).to include('first finished with exit code 1',
+                                  'Starting pipeline second',
+                                  'second finished with exit code 0')
+      end
+    end
+
+    context 'with one pipeline dependent on another' do
+      let(:dependent_success) { 'spec/supports/config/factory-dependent-success.yml' }
+      it 'runs the dependent pipeline if the dpendency finished successfully' do
+        execute("#{operate} factory #{dependent_success}")
+        expect(output).to include('first finished with exit code 0',
+                                  'second finished with exit code 0')
+      end
+
+      let(:dependent_failure) { 'spec/supports/config/factory-dependent-failure.yml' }
+      it 'does not run the dependent pipeline if the dpendency finished unsuccessfully' do
+        execute("#{operate} factory #{dependent_failure}")
+        expect(output).to include('first finished with exit code 1',
+                                  'Not running second')
+      end
+    end
+
+    # TODO: test waiting periods when changing to proper Thor tests
   end
 end
