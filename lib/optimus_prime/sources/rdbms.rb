c1ccfbafe31a578460b1f2f3c5eb6580fe59281a
Chat Lertvanasirikul <iamm@Chats-iMac.local>
2015-04-08 15:47:17 +0700

    change rdbms source

--- lib/optimus_prime/sources/rdbms.rb
+++ lib/optimus_prime/sources/rdbms.rb
@@ -2,16 +2,21 @@ module OptimusPrime
   module Sources
     class Rdbms < Source
       def initialize(dsn:, query:, **options)
-        @db = Sequel.connect(dsn, **options)
-        @result = @db[query]
+        @db ||= Sequel.connect(dsn, **options)
+        @query = query
       end
 
       def each
-        @result.each do |row|
+        query.each do |row|
           yield row
         end
       end
 
+      private
+
+      def query
+        @db[@query]
+      end
     end
   end
 end
