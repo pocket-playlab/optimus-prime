7540dbe72493253e15734a63327da0f0061a75db
Jorin Vogel <hi@jorin.me>
2015-04-08 14:00:30 +0700

    document and refactor  case_string

--- lib/optimus_prime/transformers/cast_string.rb
+++ lib/optimus_prime/transformers/cast_string.rb
@@ -1,9 +1,29 @@
+# The CastString transformer converts
+# string values in a Hash to their real types.
+#
+# Keys in the hash can be strings or symbols.
+#
+# You configure the class with a type_map.
+# All other keys are not changed.
+#
+# Example type_map with all supported types:
+#
+# {
+#   amount: 'integer',
+#   price: 'float',
+#   is_available: 'boolean',
+#   notes: 'string',
+#   due: 'date'
+# }
+#
+# With each write it accepts a Hash and and returns a Hash with converted types.
+
 module OptimusPrime
   module Transformers
     class CastString < Destination
-      # This class accepts a hash object and converts its values
-      # to their real data types based on the map of fieldname/
-      # datatype given in the initializer.
+      # type_map - Hash specifying the types like in the example above
+      # stringify - Optional Boolean you can set to false to
+      #             NOT convert Symbol keys of the type_map to Strings
       def initialize(type_map:, stringify: true)
         @type_map = stringify ? type_map.stringify_nested_symbolic_keys : type_map
       end
@@ -16,18 +36,20 @@ module OptimusPrime
       private
 
       def transform(record)
-        record.keys.each do |field|
-          next unless @type_map.include? field
-          record[field] =
-            case @type_map[field].downcase
-            when 'integer' then Integer(record[field], 10)
-            when 'float'   then Float(record[field])
-            when 'boolean' then (record[field].downcase == 'true')
-            when 'string'  then record[field] # the same
-            else raise TypeError.new("Cannot convert #{@type_map[field]} to String!")
-            end
+        # NOTE: Could also use #map here to not overwrite the existing record.
+        record.each do |key, val|
+          type = @type_map[key.to_s]
+          record[key] = case type && type.downcase
+            when nil        then val
+            when 'string'   then val
+            # Using Integer and Float as constructors to raise Exception.
+            when 'integer'  then Integer(val, 10)
+            when 'float'    then Float(val)
+            # NOTE: Every string not being 'true' results in false.
+            when 'boolean'  then val.downcase == 'true'
+            else raise TypeError.new("Cannot convert #{type} to String!")
+          end
         end
-        record
       rescue => e
         raise e if e.instance_of? TypeError # if the map has a problem, we should blow up
         logger.error("Exception handled - #{e.class}: #{e.message} - record: #{record}")
