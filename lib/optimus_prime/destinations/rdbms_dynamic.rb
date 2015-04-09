af244466f9ed83c46150650fbd8df10b145eac8d
Jorin Vogel <hi@jorin.me>
2015-04-09 15:58:57 +0700

    implemented RdbmsDynamic

---
+++ lib/optimus_prime/destinations/rdbms_dynamic.rb
@@ -0,0 +1,69 @@
+# The RdbmsDynamic source creates a new row for each given record
+# and automatically adds columns to the table as needed.
+#
+# Expects records to be formatted as Hashes.
+#
+# For each field the key is used as column name,
+# so make sure it is in the correct format.
+# The type for the column is determined from the type of the value.
+# Only use values with types supported by Sequel.
+#
+# This source won't update or delete any rows.
+#
+# For params have a look at RdbmsWriter#initialize.
+
+module OptimusPrime
+  module Destinations
+    class RdbmsDynamic < RdbmsWriter
+      def write(record)
+        super(record) if ensure_columns(record)
+      end
+
+      private
+
+      # Create missing columns.
+      # Return a boolean indicating if operation was successful.
+      def ensure_columns(record)
+        columns = missing_columns(record)
+        add_columns(columns) unless columns.empty?
+        true
+      # TODO: What's the correct Exception here to catch possible DB errors?
+      rescue StandardError => e
+        logger.error "Exception handled - #{e.class}: #{e.message} - record: #{record}"
+        false
+      end
+
+      # Find columns missing in database.
+      # Return a Hash of column names and their types
+      #
+      # Example:
+      # {
+      #   name: String,
+      #   sample: Integer
+      # }
+      def missing_columns(record)
+        table_columns = @table.columns
+        Hash[
+          record
+            .reject { |key| table_columns.include? key }
+            .map { |key, val| [key, val.class] }
+        ]
+      end
+
+      # Adds a Hash of columns to the DB.
+      # Resets @table with new schema
+      def add_columns(columns)
+        table_name = @table.opts[:from].first
+
+        @db.alter_table table_name do
+          columns.each do |name, type|
+            add_column(name, type)
+          end
+        end
+
+        # Reset table to have new schema
+        @table = @db[table_name]
+      end
+    end
+  end
+end
---
+++ spec/optimus_prime/destinations/rdbms_dynamic_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'optimus_prime/destinations/rdbms_dynamic'
+
+RSpec.describe OptimusPrime::Destinations::RdbmsDynamic do
+  let(:input) do
+    [
+      { name: 'Rick' },
+      { name: 'Omar', car: 'Range Rover' },
+      { name: 'Prair', car: 'Toyota Camry', horsepower: 160 },
+      { name: 'M', car: 'Honda Civic Type R', color: 'red' },
+      { name: 'Thibault', car: 'Audi S4' },
+      { name: 'Tamer', car: 'Mercedes SLK', horsepower: 350, color: 'blue' },
+    ]
+  end
+
+  let(:output) do
+    [
+      { id: 1, name: 'Rick', car: nil, horsepower: nil, color: nil },
+      { id: 2, name: 'Omar', car: 'Range Rover', horsepower: nil, color: nil },
+      { id: 3, name: 'Prair', car: 'Toyota Camry', horsepower: 160, color: nil },
+      { id: 4, name: 'M', car: 'Honda Civic Type R', horsepower: nil, color: 'red' },
+      { id: 5, name: 'Thibault', car: 'Audi S4', horsepower: nil, color: nil },
+      { id: 6, name: 'Tamer', car: 'Mercedes SLK', horsepower: 350, color: 'blue' }
+    ]
+  end
+
+  let(:dsn) { 'sqlite://test.db' }
+  let(:table) { :developer_cars }
+
+  let(:destination) do
+    OptimusPrime::Destinations::RdbmsDynamic.new dsn: dsn, table: table, sql_trace: false
+  end
+
+  before do
+    db = Sequel.connect(dsn)
+    # if you need to debug or trace sql, uncomment following lines
+    # db.loggers << Logger.new($stdout)
+    # db.sql_log_level = :debug
+    db.drop_table? table
+    db.create_table(table) { primary_key :id }
+  end
+
+  it 'writes all record to the database and creates columns as needed' do
+    db = Sequel.connect(dsn)
+    input.each { |record| destination.write record }
+    destination.close
+    expect(db[table].all).to eq(output)
+  end
+end
