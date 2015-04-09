289a960b0a327739ad840552a2ed78a3f76189e2
Jorin Vogel <hi@jorin.me>
2015-04-09 13:33:08 +0700

    some comments for existing rdbms destionations

--- lib/optimus_prime/destinations/rdbms.rb
+++ lib/optimus_prime/destinations/rdbms.rb
@@ -1,6 +1,15 @@
+# The Rdbms source extends RdbmsWriter.
+# It not just inserts new rows but also offers
+# the option to delete data from the database before the first insertion.
+
 module OptimusPrime
   module Destinations
     class Rdbms < RdbmsWriter
+
+      # dsn   - Connection string for the database
+      # table - Name of the table to use
+      # delete_conditions - used in the #where method to find the rows to delete
+      # options - all additional parameters are passed to Sequel
       def initialize(dsn:, table:, delete_conditions:, **options)
         @delete_conditions = delete_conditions
         @records_deleted = false
