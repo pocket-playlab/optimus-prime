8631cc51eaedb2ace3daed738d6e39c973a6240e
Rinlapat Pu <prair@rinlapat.me>
2015-05-14 16:00:24 +0700

    Add a test case for symbol keys in BigqueryNestedFieldsStringConverter spec.

--- spec/optimus_prime/transformers/bigquery_nested_fields_string_converter_spec.rb
+++ spec/optimus_prime/transformers/bigquery_nested_fields_string_converter_spec.rb
@@ -29,41 +29,7 @@ module OptimusPrime
 end
 
 describe OptimusPrime::Transformers::BigqueryNestedFieldsStringConverter do
-  let(:input) do
-    [
-      {
-        'field1' => 'value',
-        'field2' => "{\"customer_id\":298,\"type\":\"sms\",\"cost\":2}\n",
-        'field3' => "{\"customer_id\":298,\"type\":\"mms\",\"cost\":5}\n"
-      },
-      {
-        'field1' => 'value',
-        'field2' => "{\"customer_id\":555,\"type\":\"sms\",\"cost\":2}\n"
-      },
-      {
-        'field1' => 'value'
-      }
-    ]
-  end
-
-  let(:output) do
-    [
-      {
-        'field1' => 'value',
-        'field2' => [{ 'customer_id' => 298, 'type' => 'sms', 'cost' => 2 }],
-        'field3' => [{ 'customer_id' => 298, 'type' => 'mms', 'cost' => 5 }]
-      },
-      {
-        'field1' => 'value',
-        'field2' => [{ 'customer_id' => 555, 'type' => 'sms', 'cost' => 2 }]
-      },
-      {
-        'field1' =>  'value'
-      }
-    ]
-  end
-
-  let(:pipeline) do
+  def init_pipeline(keys:)
     OptimusPrime::Pipeline.new(
       **{
         src: {
@@ -73,7 +39,7 @@ describe OptimusPrime::Transformers::BigqueryNestedFieldsStringConverter do
         },
         trans: {
           class: 'OptimusPrime::Transformers::BigqueryNestedFieldsStringConverter',
-          params: { keys: ['field2', 'field3'] },
+          params: { keys: keys },
           next: ['dest']
         },
         dest: { class: 'OptimusPrime::Destinations::MyTestDestination' }
@@ -81,8 +47,77 @@ describe OptimusPrime::Transformers::BigqueryNestedFieldsStringConverter do
     )
   end
 
-  it 'converts one level nested-fields string to an array of hash' do
-    results = pipeline.start.wait.steps[:dest].written
-    expect(results).to match_array output
+  context 'input hash has string keys' do
+    let(:input) do
+      [
+        {
+          'field1' => 'value',
+          'field2' => "{\"customer_id\":298,\"type\":\"sms\",\"cost\":2}\n",
+          'field3' => "{\"customer_id\":298,\"type\":\"mms\",\"cost\":5}\n"
+        },
+        {
+          'field1' => 'value',
+          'field2' => "{\"customer_id\":555,\"type\":\"sms\",\"cost\":2}\n"
+        },
+        {
+          'field1' => 'value'
+        }
+      ]
+    end
+
+    let(:output) do
+      [
+        {
+          'field1' => 'value',
+          'field2' => [{ 'customer_id' => 298, 'type' => 'sms', 'cost' => 2 }],
+          'field3' => [{ 'customer_id' => 298, 'type' => 'mms', 'cost' => 5 }]
+        },
+        {
+          'field1' => 'value',
+          'field2' => [{ 'customer_id' => 555, 'type' => 'sms', 'cost' => 2 }]
+        },
+        {
+          'field1' =>  'value'
+        }
+      ]
+    end
+
+    it 'converts one level nested-fields string to an array of hash' do
+      results = init_pipeline(keys: ['field2', 'field3']).start.wait.steps[:dest].written
+      expect(results).to match_array output
+    end
+  end
+
+  context 'input hash has symbol keys' do
+    let(:input) do
+      [
+        {
+          field1: 'value',
+          field2: "{\"customer_id\":298,\"type\":\"sms\",\"cost\":2}\n",
+          field3: "{\"customer_id\":298,\"type\":\"mms\",\"cost\":5}\n"
+        },
+        {
+          field1: 'value'
+        }
+      ]
+    end
+
+    let(:output) do
+      [
+        {
+          field1: 'value',
+          field2: [{ 'customer_id' => 298, 'type' => 'sms', 'cost' => 2 }],
+          field3: [{ :customer_id => 298, :type => 'mms', :cost => 5 }]
+        },
+        {
+          field1: 'value'
+        }
+      ]
+    end
+
+    it 'converts one level nested-fields string to an array of hash' do
+      results = init_pipeline(keys: [:field2, :field3]).start.wait.steps[:dest].written
+      expect(results).to match_array output
+    end
   end
 end
