eef6ee8f7ad094a0e26d440fcaf876a47885052b
Rinlapat Pu <prair@rinlapat.me>
2015-05-25 15:46:51 +0700

    Add AppAnnie Product Sales source’s test cases.

---
+++ spec/optimus_prime/sources/app_annie_product_sales_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+require 'optimus_prime/sources/app_annie_product_sales'
+
+module OptimusPrime
+  module Destinations
+    class MyTestDestination < Destination
+      attr_reader :written
+      def initialize
+        @written = []
+      end
+
+      def write(record)
+        @written << record
+      end
+    end
+  end
+end
+
+describe OptimusPrime::Sources::AppAnnieProductSales do
+  describe '#each' do
+    let(:pipeline) do
+      OptimusPrime::Pipeline.new(
+        src: { class: 'OptimusPrime::Sources::AppAnnieProductSales',
+               params: {
+                  api_key: 'api_key',
+                  account_id: 'acc_id',
+                  product_id: 'prod_id',
+                  start_date: '2015-01-01',
+                  end_date: '2015-01-01',
+                  break_down: 'date+country+iap'
+               }, next: ['dest'] },
+        dest: { class: 'OptimusPrime::Destinations::MyTestDestination' }
+      )
+    end
+
+    let(:response_body) { File.read('spec/supports/app_annie/one_page_response.json') }
+    let(:response_body_1) { File.read('spec/supports/app_annie/response_1.json') }
+    let(:response_body_2) { File.read('spec/supports/app_annie/response_2.json') }
+
+    def stub_request_app_annie(body, params)
+      stub_request(:get,
+        "https://api.appannie.com/v1.2/accounts/acc_id/products/prod_id/sales?#{params}").
+        with(:headers => {
+          'Accept'=>'*/*; q=0.5, application/xml',
+          'Accept-Encoding'=>'gzip, deflate',
+          'Authorization'=>'Bearer api_key',
+          'User-Agent'=>'Ruby'
+        }).to_return(:status => 200, :body => body, :headers => {})
+    end
+
+    context 'one page response' do
+      it 'returns reponse body' do
+        stub_request_app_annie(response_body,
+          'break_down=date%2Bcountry%2Biap&end_date=2015-01-01&start_date=2015-01-01')
+
+        results = pipeline.start.wait.steps[:dest].written
+        expect(results).to match_array [JSON.parse(response_body)]
+      end
+    end
+
+    context 'multiple pages response' do
+      it 'returns response body' do
+        stub_request_app_annie(response_body_1,
+          'break_down=date%2Bcountry%2Biap&end_date=2015-01-01&start_date=2015-01-01')
+        stub_request_app_annie(response_body_2,
+          'break_down=date%20country%20iap&end_date=2015-01-01&page_index=1&start_date=2015-01-01')
+
+        results = pipeline.start.wait.steps[:dest].written
+        expect(results).to match_array [JSON.parse(response_body_1), JSON.parse(response_body_2)]
+      end
+    end
+  end
+end
