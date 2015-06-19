00a629d29b5cd0a51ecadde38f238cb17b0348d7
Thibault Denizet <thi.denizet@gmail.com>
2015-06-19 18:02:20 +0700

    Refactor Cloudstorage to BigQuery for broadcast

--- lib/optimus_prime/destinations/cloudstorage_to_bigquery.rb
+++ lib/optimus_prime/destinations/cloudstorage_to_bigquery.rb
@@ -19,7 +19,11 @@ module OptimusPrime
       end
 
       def write(tasks)
-        jobs = tasks.map { |table, uris| LoadJob.new client, logger, @config, table, uris }
+        jobs = tasks.map do |table, uris|
+          job = LoadJob.new client, logger, @config, table, uris
+          broadcast(:load_job_started, job)
+          job
+        end
         wait_for_jobs(jobs)
       end
 
@@ -27,10 +31,20 @@ module OptimusPrime
 
       def wait_for_jobs(jobs)
         while true
-          jobs = jobs.select(&:pending?)
+          jobs = jobs.select(&method(:check_status))
           return if jobs.empty?
           sleep SLEEPING_TIME
         end
+      end
+
+      def check_status(job)
+        pending = job.pending?
+      rescue LoadJobError => e
+        broadcast(:load_job_failed, job, e)
+        logger.error('Load job in BigQuery encountered a problem.', e)
+      ensure
+        broadcast(:load_job_finished, job) unless pending
+        pending
       end
 
       def client
@@ -47,7 +61,7 @@ module OptimusPrime
 
       class LoadJob
         # for BigQueryTableBase
-        attr_reader :client, :logger, :id, :project_id, :dataset_id, :resource
+        attr_reader :client, :logger, :id, :project_id, :dataset_id, :resource, :job_id
 
         def initialize(client, logger, config, table, uris)
           @client     = client
@@ -57,11 +71,12 @@ module OptimusPrime
           @dataset_id = config[:dataset]
           @schema     = config[:schema]
           @resource   = generate_resource
+          @uris = uris
 
           # NOTE: Could be optimised to just fetch the table once
           patch_table if exists?
 
-          insert_request = insert_files(uris)
+          insert_request = insert_files
           @job_id = JSON.parse(insert_request.body)['jobReference']['jobId']
           logger.info "LoadJob created #{@job_id} (table: #{id})."
         end
@@ -72,6 +87,7 @@ module OptimusPrime
           error = body['status']['errorResult']
 
           raise LoadJobError.new(body) if error
+
           state = body['status']['state'].downcase
           logger.info "LoadJob #{@job_id} (table: #{id}) is #{state}."
           state != 'done'
@@ -85,15 +101,15 @@ module OptimusPrime
           { schema: @schema }.stringify_nested_symbolic_keys
         end
 
-        def insert_files(uris)
-          execute(bigquery.jobs.insert, body: generate_job_data(uris))
+        def insert_files
+          execute(bigquery.jobs.insert, body: generate_job_data)
         end
 
-        def generate_job_data(uris)
+        def generate_job_data
           {
             configuration: {
               load: {
-                sourceUris: uris,
+                sourceUris: @uris,
                 schema: @schema,
                 sourceFormat: SOURCE_FORMAT,
                 destinationTable: {
