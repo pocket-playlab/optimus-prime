557f990ab815c5a333e47233cd9fb9a2c7f65736
Tamer Shlash <mr.tamershlash@gmail.com>
2015-07-06 15:10:34 +0700

    refactored the tests and used the step helpers

--- spec/optimus_prime/transformers/buffer_spec.rb
+++ spec/optimus_prime/transformers/buffer_spec.rb
@@ -1,70 +1,33 @@
 require 'spec_helper'
+require 'step_helpers'
 require 'optimus_prime/transformers/buffer'
 
 RSpec.describe OptimusPrime::Transformers::Buffer do
-  def output_of(input)
-    output = []
-    transformer.output << output
-    input.each { |record| transformer.write(record) }
-    output
-  end
+  let(:buffer) { OptimusPrime::Transformers::Buffer.new }
 
-  let(:source_a) do
-    {
-      id: 1,
-      name: 'Alice'
-    }
-  end
-
-  let(:source_b) do
-    {
-      id: 2,
-      name: 'Bob'
-    }
-  end
-
-  let(:source_c) do
-    {
-      id: 3,
-      pet: 'cat',
-      age: 4
-    }
-  end
-
-  def output_of(input)
-    buffer = OptimusPrime::Transformers::Buffer.new
-    output = []
-    buffer.output << output
-    input.each { |record| buffer.write(record) }
-    buffer.finish
-    output
-  end
-
-  context 'sources are same keys' do
-    let(:output_ab) do
+  context 'when records have the same keys' do
+    let(:sample) do
       [
         { id: 1, name: 'Alice' },
-        { id: 2, name: 'Bob' },
+        { id: 2, name: 'Bob' }
       ]
     end
 
-    it 'keeps everything' do
-      result = output_of([source_a, source_b])
-      expect(result).to match_array output_ab
+    it 'buffers everything' do
+      expect(buffer.run_with(sample.dup)).to match_array sample
     end
   end
 
-  context 'sources are difference keys' do
-    let(:output_ac) do
+  context 'when records have different keys' do
+    let(:sample) do
       [
         { id: 1, name: 'Alice' },
-        { id: 3, pet: 'cat', age: 4 },
+        { id: 3, pet: 'cat', age: 4 }
       ]
     end
 
-    it 'keeps everything' do
-      result = output_of([source_a, source_c])
-      expect(result).to match_array output_ac
+    it 'buffers everything' do
+      expect(buffer.run_with(sample.dup)).to match_array sample
     end
   end
 end
