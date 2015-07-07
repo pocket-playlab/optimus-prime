54f088bcae14ccb85418afa5ea912155a4aa58db
Tamer Shlash <mr.tamershlash@gmail.com>
2015-07-07 14:55:40 +0700

    add more helpers and refactor existing ones

--- spec/step_helpers.rb
+++ spec/step_helpers.rb
@@ -1,45 +1,58 @@
-module OptimusPrime
-  class Step
-    def prepare_output(*inputs)
-      inputs.each { |i| input << i }
-      output = []
-      self.output << output
-      output
+module StepHelpers
+  def prepare_output(*inputs)
+    inputs.each { |i| input << i }
+    output = []
+    self.output << output
+    output
+  end
+
+  def run_with(*inputs)
+    output = prepare_output(*inputs)
+    self.start.join # will be closed automatically if everything is OK
+    output.compact
+  end
+
+  def run_and_close(*inputs)
+    output = prepare_output(*inputs)
+    self.start.join
+    output.compact
+  ensure # closing the step (and call finish) if not closed automatically
+    close unless closed?
+  end
+
+  def run_and_raise(*inputs)
+    output = prepare_output(*inputs)
+    self.start.join
+    output.compact
+  rescue
+    threads.each(&:kill)
+    raise
+  ensure
+    close
+  end
+
+  def log_to(logger)
+    self.logger = logger
+    self
+  end
+
+  def log_to_stdout
+    with_logger(STDOUT)
+  end
+
+  class << self
+    def create_with_logger(logger, **config)
+      create(**config).log_to(logger)
     end
 
-    def run_with(*inputs)
-      output = prepare_output(*inputs)
-      start
-      join
-      return output.compact
-    ensure
-      close
-    end
-
-    def log_to(logger)
-      self.logger = logger
-      self
-    end
-
-    def log_to_stdout
-      with_logger(STDOUT)
-    end
-
-    class << self
-      def create_with_logger(logger, **config)
-        create(**config).log_to(logger)
-      end
-
-      def create_with_stdout(**config)
-        create(**config).log_to_stdout
-      end
+    def create_with_stdout(**config)
+      create(**config).log_to_stdout
     end
   end
 end
 
-module StepHelpers
-end
-
-RSpec.configure do |config|
-  config.include StepHelpers
+module OptimusPrime
+  class Step
+    include StepHelpers
+  end
 end
