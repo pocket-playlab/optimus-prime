1ec12a21d461e5f3e85c3522440b4db4a9bebcbe
Tamer Shlash <mr.tamershlash@gmail.com>
2015-07-07 17:15:03 +0700

    make underscore keys use step helpers

--- spec/optimus_prime/transformers/underscore_key.rb
+++ spec/optimus_prime/transformers/underscore_key.rb
@@ -9,21 +9,18 @@ RSpec.describe OptimusPrime::Transformers::UnderscoreKey do
     ]
   end
 
-  let(:result) do
+  let(:output) do
     [
       { 'name' => 'John', 'this_is_my_size' => 192.3, 'meme' => 83.5 },
       { '2015_12_01_12_15' => 'Jack', '3_a' => 24 }
     ]
   end
 
-  let(:transformer) { OptimusPrime::Transformers::UnderscoreKey.new }
+  let(:step) { OptimusPrime::Transformers::UnderscoreKey.new }
 
   context 'Multiple Hashes with different combinations of characters' do
     it 'should return correct formatted versions' do
-      output = []
-      transformer.output << output
-      input.each { |record| transformer.write(record) }
-      expect(output).to match_array result
+      expect(step.run_with(input)).to match_array output
     end
   end
 end
