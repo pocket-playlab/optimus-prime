9012ffc4bbb839ef693116c7b99a87ca37c74652
Chat Lertvanasirikul <iamm@Chats-iMac-5.local>
2015-07-07 11:51:35 +0700

    fix error

--- lib/optimus_prime/sources/events_collector.rb
+++ lib/optimus_prime/sources/events_collector.rb
@@ -10,17 +10,18 @@ module OptimusPrime
         objects.each do |object|
           gz = Zlib::GzipReader.new object.body
           gz.each do |line|
-            parse(line)
+            row = parse(line)
+            yield row if row
           end
         end
       end
 
       def parse(line)
         begin
-          yield JSON.parse line
+          JSON.parse line
         rescue => e
           logger.error "#{e.class} #{e.message} : Cannot parse the line #{line}"
-          next
+          nil
         end
       end
     end
