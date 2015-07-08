d8fd9329d8c5db6631960c2c439416ddadb72420
Tamer Shlash <mr.tamershlash@gmail.com>
2015-07-08 13:54:30 +0700

    make csv dest specs use step helpers

--- spec/optimus_prime/destinations/csv_spec.rb
+++ spec/optimus_prime/destinations/csv_spec.rb
@@ -2,18 +2,16 @@ require 'spec_helper'
 require 'optimus_prime/destinations/csv'
 
 RSpec.describe OptimusPrime::Destinations::Csv do
-  let(:options) { { endpoint: 'http://localhost:10001/', force_path_style: true } }
-
-  let(:s3) { Aws::S3::Client.new options }
-
-  let(:bucket) { 'ppl-csv-test' }
-
-  let(:destination) do
-    OptimusPrime::Destinations::Csv.new fields: ['name', 'age'],
-                                        bucket: bucket,
-                                        key: 'people.csv',
-                                        **options
+  let(:params) do
+    {
+      fields: ['name', 'age'],
+      bucket: 'ppl-csv-test',
+      key: 'people.csv',
+    }
   end
+  let(:options) { { endpoint: 'http://localhost:10001/', force_path_style: true } }
+  let(:s3) { Aws::S3::Client.new options }
+  let(:step) { OptimusPrime::Destinations::Csv.new(**params.merge(options))}
 
   let(:input) do
     [
@@ -21,51 +19,30 @@ RSpec.describe OptimusPrime::Destinations::Csv do
       { 'name' => 'Alice', 'age' => 34, 'likes' => 'durian' },
     ]
   end
+  let(:output) { [{ "name" => "Bob", "age" => 28 }, { "name" => "Alice", "age" => 34 }] }
 
-  before :each do
-    s3.create_bucket bucket: bucket
-  end
-
-  def upload
-    input.each { |obj| destination.write obj }
-    destination.close
-  end
-
-  def download
-    object = s3.get_object bucket: bucket, key: destination.key
-    CSV.new object.body, converters: :all
-  end
-
-  def expected
-    input.map do |row|
-      row.select { |k, v| destination.fields.include? k }
-    end
-  end
+  before(:each) { s3.create_bucket bucket: params[:bucket] }
 
   def parse(csv)
-    csv.map { |row| destination.fields.zip(row).to_h }
+    csv.map { |row| params[:fields].zip(row).to_h }
   end
 
   def test
-    upload
-    csv = download
+    step.run_with(input.dup)
+    csv = CSV.new(s3.get_object(bucket: params[:bucket], key: params[:key]).body, converters: :all)
     header = csv.first
-    expect(header).to eq destination.fields
-    expect(parse csv).to eq expected
+    expect(header).to match_array params[:fields]
+    expect(parse csv).to match_array output
   end
 
-  it 'should upload csv to s3' do
-    test
-  end
+  it('uploads csv to s3') { test }
 
-  it 'should upload csv to s3 in chunks' do
+  it 'uploads csv to s3 in chunks' do
     options[:chunk_size] = 5
     test
   end
 
-  it 'should send bucket and key when it finish' do
-    s3_connect = { bucket: bucket, key: 'people.csv' }
-    expect(destination).to receive(:push).with(s3_connect)
-    destination.finish
+  it 'pushes bucket and key when it finishes' do
+    expect(step.run_with([])).to match_array [{ bucket: params[:bucket], key: params[:key] }]
   end
 end
