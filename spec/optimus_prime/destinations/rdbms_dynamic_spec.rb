26fbe0fdf8d7768de48ba114ffe1c9ec6b1d0bc1
Tamer Shlash <mr.tamershlash@gmail.com>
2015-07-09 12:02:04 +0700

    make rdbms dynamic use step helpers

--- spec/optimus_prime/destinations/rdbms_dynamic_spec.rb
+++ spec/optimus_prime/destinations/rdbms_dynamic_spec.rb
@@ -15,23 +15,20 @@ RSpec.describe OptimusPrime::Destinations::RdbmsDynamic do
 
   let(:output) do
     [
-      { id: 1, name: 'Rick', car: nil, horsepower: nil, color: nil },
-      { id: 2, name: 'Omar', car: 'Range Rover', horsepower: nil, color: nil },
-      { id: 3, name: 'Prair', car: 'Toyota Camry', horsepower: 160, color: nil },
-      { id: 4, name: 'M', car: 'Honda Civic Type R', horsepower: nil, color: 'red' },
-      { id: 5, name: 'Thibault', car: 'Audi S4', horsepower: nil, color: nil },
-      { id: 6, name: 'Tamer', car: 'Mercedes SLK', horsepower: 350, color: 'blue' }
+      { id: 6, name: 'Rick', car: nil, horsepower: nil, color: nil },
+      { id: 5, name: 'Omar', car: 'Range Rover', horsepower: nil, color: nil },
+      { id: 4, name: 'Prair', car: 'Toyota Camry', horsepower: 160, color: nil },
+      { id: 3, name: 'M', car: 'Honda Civic Type R', horsepower: nil, color: 'red' },
+      { id: 2, name: 'Thibault', car: 'Audi S4', horsepower: nil, color: nil },
+      { id: 1, name: 'Tamer', car: 'Mercedes SLK', horsepower: 350, color: 'blue' }
     ]
   end
 
   let(:dsn) { 'sqlite://test.db' }
   let(:table) { :developer_cars }
+  let(:step) { OptimusPrime::Destinations::RdbmsDynamic.new(dsn: dsn, table: table, sql_trace: false) }
 
-  let(:destination) do
-    OptimusPrime::Destinations::RdbmsDynamic.new dsn: dsn, table: table, sql_trace: false
-  end
-
-  before do
+  before :each do
     db = Sequel.connect(dsn)
     # if you need to debug or trace sql, uncomment following lines
     # db.loggers << Logger.new($stdout)
@@ -42,8 +39,7 @@ RSpec.describe OptimusPrime::Destinations::RdbmsDynamic do
 
   it 'writes all record to the database and creates columns as needed' do
     db = Sequel.connect(dsn)
-    input.each { |record| destination.write record }
-    destination.close
-    expect(db[table].all).to eq(output)
+    step.run_with(input)
+    expect(db[table].all).to match_array output
   end
 end
