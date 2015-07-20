729fa56a190b75f43d42e7730fdda855125480f8
Chat Lertvanasirikul <iamm@Chats-iMac-43.local>
2015-07-20 15:21:35 +0700

    Add ability to extract data from AppAnnie

---
+++ lib/optimus_prime/transformers/extract_app_annie_data.rb
@@ -0,0 +1,59 @@
+module OptimusPrime
+  module Transformers
+    # This class create for flatten nested json from AppAnnie to single hash.
+    # This class rewrite from ExtractAppAnnieProductSales to make more flexible for "default_fields" and "extract_keys" variables
+      # default_fields: specify whatever fields that you need the data.
+      # extract_keys: specify the nested key to drill down and flatten data into the single hash
+    # See example in spec and PR
+
+    class ExtractAppAnnieData < OptimusPrime::Destination
+      def initialize(default_fields:, extract_keys:)
+        @rows = []
+        @default_fields = default_fields
+        @extract_keys = extract_keys
+      end
+
+      def write(data)
+        extract data
+      end
+
+      def finish
+        @rows.each { |row| push row }
+      end
+
+      private
+
+      def extract(data)
+        @extract_keys.each do |key|
+          extract_list data, key
+        end
+      end
+
+      def expand_hash(data, prefix)
+        new_row = {}
+        if data.is_a? Hash
+          data.each { |key, value| set_new_key(new_row, value, "#{prefix}_#{key}") }
+        else
+          new_row["#{prefix}"] = data
+        end
+        new_row
+      end
+
+      def set_new_key(row, value, key)
+        value.is_a?(Hash) ? value.each { |k, v| row["#{key}_#{k}"] = v } : row[key] = value
+      end
+
+      def default_fields(data)
+        data.select { |k, v| @default_fields.include? k }
+      end
+
+      def extract_list(data, prefix)
+        data[prefix].each do |row|
+          new_row = default_fields(data).merge('sales_type' => prefix)
+          row.each { |key, value| new_row.merge! expand_hash(value, key) }
+          @rows << new_row
+        end
+      end
+    end
+  end
+end
---
+++ spec/optimus_prime/transformers/extract_app_annie_data_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe OptimusPrime::Transformers::ExtractAppAnnieData do
+  let(:basepath) { 'app_annie' }
+  let(:step) { OptimusPrime::Transformers::ExtractAppAnnieData.new(
+      default_fields: [ 'vertical', 'currency', 'market' ],
+      extract_keys: ['sales_list', 'iap_sales_list']
+    )
+  }
+
+  context 'input contains both sales_list and iap_sales_list from multiple pages' do
+    let(:input)  { load_json("#{basepath}/input_multiple.json")  }
+    let(:output) { load_json("#{basepath}/output_multiple.json") }
+
+    it 'returns an array of extracted data' do
+      expect(step.run_with(input)).to match_array output
+    end
+  end
+
+  context 'input contains only sales_list from one page' do
+    let(:input)  { load_json("#{basepath}/input_sales_list.json")  }
+    let(:output) { load_json("#{basepath}/output_sales_list.json") }
+
+    it 'returns an array of extracted data' do
+      expect(step.run_with(input)).to match_array output
+    end
+  end
+
+  context 'input contains only iap_sales_list from one page' do
+    let(:input)  { load_json("#{basepath}/input_iap_sales_list.json")  }
+    let(:output) { load_json("#{basepath}/output_iap_sales_list.json") }
+
+    it 'returns an array of extracted data' do
+      expect(step.run_with(input)).to match_array output
+    end
+  end
+end
