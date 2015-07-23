55f1936d3281625b1105eb52bcad91d27463c382
Jeffrey Kehler <jeffrey.k@pocketplaylab.com>
2015-07-23 13:27:51 +0700

    Added 500 and 503 retry and exception handling

--- lib/optimus_prime/transformers/geoip_lookup.rb
+++ lib/optimus_prime/transformers/geoip_lookup.rb
@@ -7,6 +7,8 @@
 # Parameters:
 #   :ip_field - Field in the record containing the IP Address.
 #   :api_url  - API Endpoint for the freegeoip server.
+#   :num_retry - Number of times a 503 Service Unavailable, or
+#                500 Internal Server Error attempt should be retried
 #
 # Output:
 # {
@@ -23,6 +25,9 @@
 #   'geo_longitude' => 37.616,
 #   'geo_metro_code' => 0
 # }
+#
+# If there was a non 503 or 500 error then the record will be returned with
+# the geo_ fields ommitted and the error will be logged
 
 require 'rest_client'
 require 'json'
@@ -31,21 +36,39 @@ module OptimusPrime
   module Transformers
     class GeoIP < Destination
 
-      def initialize(ip_field:, api_url:)
+      def initialize(ip_field:, api_url:, num_retry: 3)
         @ip_field = ip_field
         @api_url = api_url
+        @num_retry = num_retry
       end
 
       def write(record)
-        push lookup_geoip record
+        push get_geoip record
       end
 
       private
 
-      def lookup_geoip(record)
-        resp = JSON.parse(RestClient.get(@api_url + record[@ip_field]).body)
-        resp.each do |key, value|
-          record["geo_#{key}"] = value
+      def get_geoip(record)
+        retry_count = 0
+
+        while retry_count < @num_retry
+          RestClient.get(@api_url + record[@ip_field]) do |response, request, result|
+            case response.code
+            when 200
+              JSON.parse(response.body).each do |key, value|
+                record["geo_#{key}"] = value
+              end
+              return record
+            when 503, 500
+              retry_count += 1
+              if retry_count == @num_retry
+                raise IOError, "Geoip service unavailable - code: #{response.code} - record: #{record}"
+              end
+            else
+              logger.error("Geoip lookup failed - code: #{response.code} - record: #{record}")
+              return record
+            end
+          end
         end
         record
       end
--- spec/optimus_prime/transformers/geoip_lookup_spec.rb
+++ spec/optimus_prime/transformers/geoip_lookup_spec.rb
@@ -3,19 +3,25 @@ require 'spec_helper'
 RSpec.describe OptimusPrime::Transformers::GeoIP do
   let(:ip_field) { 'ip_addr' }
   let(:api_url) { 'https://freegeoip.net/json/' }
+  let(:num_retry) { 3 }
 
-  let(:response_1) { File.read 'spec/supports/geoip/response_1.json' }
+  let(:stub_url) { api_url + '188.32.194.21' }
+  let(:logfile) { '/tmp/geoip_lookup.log' }
+  let(:logger) { Logger.new(logfile) }
+
+  let(:response_success) { File.read('spec/supports/geoip/response_1.json') }
+  let(:response_404) { File.read('spec/supports/geoip/404_response.txt') }
 
   let(:input) do
     [
       {
-        'name' => 'test',
-        'ip_addr' => '188.32.194.21'
+          'name' => 'test',
+          'ip_addr' => '188.32.194.21'
       }
     ]
   end
 
-  let(:output) do
+  let(:success_output) do
     [
       {
         'name' => 'test',
@@ -35,15 +41,50 @@ RSpec.describe OptimusPrime::Transformers::GeoIP do
     ]
   end
 
-  before do
-    stub_request(:get, api_url + "188.32.194.21")
-      .to_return(status: 200, body: response_1)
+  let(:failed_output) do
+    [
+      {
+        'name' => 'test',
+        'ip_addr' => '188.32.194.21',
+      }
+    ]
   end
 
-  context 'lookup geoip information' do
-    it 'adds geo_* fields with lookup values' do
-      step = OptimusPrime::Transformers::GeoIP.new(ip_field: ip_field, api_url: api_url)
-      expect(step.run_with(input)).to match_array output
+  let(:step) do
+      OptimusPrime::Transformers::GeoIP.new(ip_field: ip_field, api_url: api_url, num_retry: 3).log_to(logger)
+  end
+
+  context 'valid geoip lookup' do
+    before do
+      stub_request(:get, stub_url)
+        .to_return(status: 200, body: response_success)
     end
+
+    it 'adds geo_* fields with lookup values' do
+      expect(step.run_with(input)).to match_array success_output
+    end
+  end
+
+  context 'geoip lookup 404' do
+    before do
+      stub_request(:get, stub_url)
+        .to_return(status: 404, body: response_404)
+    end
+
+    it 'does not add geo_ fields' do
+      expect(step.run_with(input)).to match_array failed_output
+    end
+  end
+
+  context 'geoip lookup unavailable' do
+    before do
+      stub_request(:get, stub_url)
+        .to_return(status: 503)
+    end
+
+    it 'raises an exception' do
+      expect { step.run_with(input) }.to raise_error(IOError)
+    end
+
   end
 end
