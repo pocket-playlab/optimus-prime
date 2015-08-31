7abe7153908eaf89537c8e0071c525b984381b6f
Chat Lertvanasirikul <iamm@Chats-iMac-10.local>
2015-08-31 18:22:09 +0700

    remove csv parser from AppsFlyer class

--- spec/optimus_prime/sources/appsflyer_spec.rb
+++ spec/optimus_prime/sources/appsflyer_spec.rb
@@ -18,39 +18,6 @@ describe OptimusPrime::Sources::Appsflyer do
   end
 
   it 'should yield events' do
-    source.run_with.each do |event|
-      expect(event.keys).to match_array [
-        'Agency/PMD (af_prt)',
-        'App Version',
-        'Appsflyer Device Id',
-        'Campaign (c)',
-        'City',
-        'Click Time',
-        'Click URL',
-        'Contributor 1',
-        'Contributor 2',
-        'Contributor 3',
-        'Cost Per Install (af_cpi)',
-        'Country Code',
-        'Customer User Id',
-        'Device Name',
-        'Device Type',
-        'IDFA',
-        'IP',
-        'Install Time',
-        'Language',
-        'MAC',
-        'Media Source (pid)',
-        'OS Version',
-        'SDK Version',
-        'Site Id (af_siteid)',
-        'Sub Param 1 (af_sub1)',
-        'Sub Param 2 (af_sub2)',
-        'Sub Param 3 (af_sub3)',
-        'Sub Param 4 (af_sub4)',
-        'Sub Param 5 (af_sub5)',
-        'WIFI'
-      ]
-    end
+    expect(source.run_with.first).to eq(installs_report.force_encoding('ASCII-8BIT'))
   end
 end
